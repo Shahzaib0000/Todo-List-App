0fa71431b867d0325934046d1d8bec7f42634f2f
= <shahzaibawan085@gmail.com>
2021-08-29 17:11:20 +0500

    edit function updated

--- app.js
+++ app.js
@@ -2,38 +2,48 @@ var list = document.getElementById("list")
 
 function addTodo() {
     var input = document.getElementById("todo-input");
-    if (input.value == "." && input.value == "           ") {
-        alert("invalid input")
-        
+    var val = input.value;
+    if (val == "" || val == " " || val == "  ") {
+        alert("Invalid Input")
+        return 0
     }
+    else if (!val.trim().length) {
+        input.value = ""
+        return 0
+    }
+    else {
+        // Create li 
+        var li = document.createElement("li");
+        var liText = document.createElement("input");
+        
+        liText.setAttribute('disabled', 'disabled');
+        liText.setAttribute('class','inputBox')
+        liText.value = val.toUpperCase();
+        li.appendChild(liText)
 
-    // Create li 
-    var li = document.createElement("li");
-    var textNode = document.createTextNode(input.value)
-    li.appendChild(textNode)
-
-    // Creating edit button
-    var editbtn = document.createElement("button");
-    var editText = document.createTextNode("EDIT");
-    editbtn.appendChild(editText)
-    editbtn.setAttribute("class", "edit-btn");
-    editbtn.setAttribute("value", "Edit");
-    editbtn.setAttribute("onclick", "editButton(this)");
-    li.appendChild(editbtn)
+        // Creating edit button
+        var editbtn = document.createElement("button");
+        var editText = document.createTextNode("EDIT");
+        editbtn.appendChild(editText)
+        editbtn.setAttribute("class", "edit-btn");
+        editbtn.setAttribute("value", "Edit");
+        editbtn.setAttribute("onclick", "editButton(this.previousSibling)");
+        li.appendChild(editbtn)
 
 
-    // Creating delete button
-    var dltbtn = document.createElement("button");
-    var dltText = document.createTextNode("DELETE");
-    dltbtn.appendChild(dltText)
-    dltbtn.setAttribute("class", "dlt-btn");
-    dltbtn.setAttribute("value", "delete");
-    dltbtn.setAttribute("onclick", "dltButton(this)");
-    li.appendChild(dltbtn)
-
-    // apdated ul
-    list.appendChild(li)
-    input.value = ""
+        // Creating delete button
+        var dltbtn = document.createElement("button");
+        var dltText = document.createTextNode("DELETE");
+        dltbtn.appendChild(dltText)
+        dltbtn.setAttribute("class", "dlt-btn");
+        dltbtn.setAttribute("value", "delete");
+        dltbtn.setAttribute("onclick", "dltButton(this)");
+        dltbtn.setAttribute('id', 'delete')
+        li.appendChild(dltbtn)
+        // apdated ul
+        list.appendChild(li)
+        input.value = ""
+    }
 }
 
 function dltAll() {
@@ -44,21 +54,20 @@ function dltButton(e) {
     e.parentNode.remove()
 }
 
-function editButton(e) {
+function editButton(listData) {
+    var del = document.getElementById("delete");
+    var check = listData.nextSibling.innerText;
+    console.log(check)
+    if (check == "EDIT") {
+        listData.disabled = false;
+        listData.focus();
+        listData.nextSibling.innerText = "Update";
+        del.disabled = true;
 
-    var todoItem = e.parentNode.firstChild.nodeValue;
-    var edit = prompt("Edit todo here:", todoItem);
-    e.parentNode.firstChild.nodeValue = edit
-
+    }
+    else if (check === "Update") {
+        listData.disabled = true;
+        listData.nextSibling.innerText = "Edit";
+        del.disabled = false;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
